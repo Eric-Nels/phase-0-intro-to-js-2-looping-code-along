ab543946316890795b5f4ed3403b0d14b5ef1a04
Eric Nelson <ernelson12o0@gmail.com>
2023-01-30 20:05:47 -0800

    Lab Complete

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+function writeCards(name, event) {
+    let message = [];
+    for (let i = 0; i < name.length; i++) {
+        message.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`); 
+    }
+ 
+    return message;
+}
+
+
+function countDown(positiveInteger) {
+    while (positiveInteger >= 0) {
+        console.log(positiveInteger); 
+        positiveInteger-- 
+    } 
+    
+}
